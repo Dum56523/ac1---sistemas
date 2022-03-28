44d8febe0bd272948a8fd4f41194fbd71c14f8b4
Augusto Costa <augusto.costa@aluno.faculdadeimpacta.com.br>
2022-03-28 20:45:29 -0300

    a

--- sistemas.sql
+++ sistemas.sql
@@ -1,21 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Tempo de geração: 28-Mar-2022 às 19:03
--- Versão do servidor: 5.7.31
--- versão do PHP: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Banco de dados: sistemas
@@ -87,6 +73,4 @@ INSERT INTO motorista (id, nome, cpf, placa, pais, estado, municipio, cep, logra
 (21, 'test6', '12358733544', 'asd1234', 'test6', 'test6', 'test6', '123790731', 'test6', '123', 'ap 141', 'test6@hotmail.com', 'test6');
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
